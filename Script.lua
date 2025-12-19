44fb17eca2e565452d473c8b72ccc93afa333ce4
Aadityaaaa <aadikalmekh@outlook.com>
2025-12-19 14:12:51 +0530

    Add files via upload

--- Script.lua
+++ Script.lua
@@ -1,68 +1,35 @@
 function Initialize()
-    MeasureYear = SKIN:GetMeasure('MeasureYear')
-    MeasureMonth = SKIN:GetMeasure('MeasureMonth')
     UpdateGrid()
 end
 
 function UpdateGrid()
-    -- Safety check: if measures are missing, stop to prevent crash
-    if not MeasureYear or not MeasureMonth then return end
-
-    local year = tonumber(MeasureYear:GetString())
-    local month = tonumber(MeasureMonth:GetString())
-    local daysInMonth = GetDaysInMonth(month, year)
-
     for i = 1, 31 do
-        if i > daysInMonth then
-            SKIN:Bang('!HideMeter', 'MeterDay'..i)
-            SKIN:Bang('!HideMeter', 'MeterDate'..i)
+        SKIN:Bang('!ShowMeter', 'MeterDay' .. i)
+        SKIN:Bang('!ShowMeter', 'MeterDate' .. i)
+
+        local state = tonumber(SKIN:GetVariable('Day' .. i, '0'))
+
+        if state == 1 then
+            SKIN:Bang('!SetOption', 'MeterDay' .. i, 'MeterStyle', 'StyleDone')
+            SKIN:Bang('!SetOption', 'MeterDate' .. i, 'FontColor', '0,0,0,255')
         else
-            SKIN:Bang('!ShowMeter', 'MeterDay'..i)
-            SKIN:Bang('!ShowMeter', 'MeterDate'..i)
-            
-            local state = tonumber(SKIN:GetVariable('Day'..i))
-            if state == 1 then
-                SKIN:Bang('!SetOption', 'MeterDay'..i, 'MeterStyle', 'StyleDone')
-                SKIN:Bang('!SetOption', 'MeterDate'..i, 'FontColor', '0,0,0,255')
-            else
-                SKIN:Bang('!SetOption', 'MeterDay'..i, 'MeterStyle', 'StyleTodo')
-                SKIN:Bang('!SetOption', 'MeterDate'..i, 'FontColor', '0,0,0,0')
-            end
+            SKIN:Bang('!SetOption', 'MeterDay' .. i, 'MeterStyle', 'StyleTodo')
+            SKIN:Bang('!SetOption', 'MeterDate' .. i, 'FontColor', '0,0,0,0')
         end
     end
-    
+
     SKIN:Bang('!UpdateMeter', '*')
     SKIN:Bang('!Redraw')
 end
 
 function Toggle(dayIndex)
-    local currentState = tonumber(SKIN:GetVariable('Day'..dayIndex))
-    local newState = 0
-    if currentState == 0 then newState = 1 else newState = 0 end
+    local currentState = tonumber(SKIN:GetVariable('Day' .. dayIndex, '0'))
+    local newState = (currentState == 0) and 1 or 0
 
-    SKIN:Bang('!SetVariable', 'Day'..dayIndex, newState)
-    local fullPath = SKIN:GetVariable('CURRENTPATH') .. 'Variables.inc'
-    SKIN:Bang('!WriteKeyValue "Variables" "Day'..dayIndex..'" "'..newState..'" "'..fullPath..'"')
+    SKIN:Bang('!SetVariable', 'Day' .. dayIndex, newState)
 
-    if newState == 1 then
-        SKIN:Bang('!SetOption', 'MeterDay'..dayIndex, 'MeterStyle', 'StyleDone')
-        SKIN:Bang('!SetOption', 'MeterDate'..dayIndex, 'FontColor', '0,0,0,255')
-    else
-        SKIN:Bang('!SetOption', 'MeterDay'..dayIndex, 'MeterStyle', 'StyleTodo')
-        SKIN:Bang('!SetOption', 'MeterDate'..dayIndex, 'FontColor', '0,0,0,0')
-    end
-    
-    SKIN:Bang('!UpdateMeter', 'MeterDay'..dayIndex)
-    SKIN:Bang('!UpdateMeter', 'MeterDate'..dayIndex)
-    SKIN:Bang('!Redraw')
-end
+    local path = SKIN:GetVariable('CURRENTPATH') .. 'Variables.inc'
+    SKIN:Bang('!WriteKeyValue', 'Variables', 'Day' .. dayIndex, newState, path)
 
-function GetDaysInMonth(month, year)
-    local days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-    if month == 2 then
-        if year % 4 == 0 and (year % 100 ~= 0 or year % 400 == 0) then
-            return 29
-        end
-    end
-    return days[month]
+    UpdateGrid()
 end
